641a36e10788eee0606a52a71bb192f049515054
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-03-31 10:23:21 +0530

    new file

---
+++ lib/pip_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class PIPView extends StatefulWidget {
+  const PIPView({super.key});
+
+  @override
+  State<PIPView> createState() => _PIPViewState();
+}
+
+class _PIPViewState extends State<PIPView> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
