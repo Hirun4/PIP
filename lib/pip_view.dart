d6dea6678ac51f7a10f8c8d1b55f544406f23ee5
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-04-03 10:23:21 +0530

    home screen

--- lib/pip_view.dart
+++ lib/pip_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pip_view/pip_view.dart';
 
 class PIPView extends StatefulWidget {
   const PIPView({super.key});
@@ -11,8 +12,27 @@ class _PIPViewState extends State<PIPView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("PIP View"),),
-      body: ,
+      appBar: AppBar(
+        title: Text("PIP View"),
+      ),
+      body: HomeScreen(),
     );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return PIPView(builder: (context, isFloating) {
+      return Scaffold(
+        resizeToAvoidBottomInset: !isFloating,
+        body: SafeArea(
+            child: Center(
+          child: Padding(padding: EdgeInsets.all(16)),
+        )),
+      );
+    });
   }
 }
